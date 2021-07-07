cda1eab3fc917ef190bb251d01318d5a42c47f50
gerry <gerry.baird@uk.ibm.com>
2021-07-07 15:49:34 +0100

    Initial commit.

---
+++ src/main/java/com/ibm/expiryclient/ExpiryClientApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.expiryclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpiryClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpiryClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ibm/expiryclient/ExpiryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.expiryclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpiryClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
